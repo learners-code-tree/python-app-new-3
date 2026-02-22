47cb5c1b35b6d0c7be7a1b27dc6870c4a88b4319
Scaffolder <scaffolder@backstage.io>
2026-02-22 04:13:29 +0000

    initial commit

---
+++ docs/index.md
@@ -0,0 +1,13 @@
+# Documents for python-app-new-3
+
+This application servers two endpoints:
+- `/api/v1/info`
+- `/api/v1/health`
+
+Here you could expand with details of what the two endpoints do.
+
+
+# How to access the app?
+
+You can access the app by accessing this URL: 
+`python-app-new-3-dev.test.com/api/v1/info`
---
+++ src/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+
+def info():
+#    return '<h1>Hello World</h1>'
+    return jsonify({
+        'message': 'hello_new_world',
+        'hostname': socket.gethostname(),
+        'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'real_hostname': 'newserver',
+        'deployed_on': 'kubernetes',
+        'env': 'dev',
+        'app_name': 'python-app-new-3'
+    })
+
+@app.route('/api/v1/health')
+
+def health():
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+    # app.run()
+    app.run(host="0.0.0.0")
+
+
+# '/api/v1/details'
+# '/api/v1/healthz'
+# 'message': 'hello_world' - change to 'hello_new_world'
